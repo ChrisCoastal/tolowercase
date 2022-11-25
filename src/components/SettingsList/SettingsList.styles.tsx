89e0950c2b357457fea6f152f7156536c5ec04b7
ChrisCoastal <contactchriscoastal@gmail.com>
2022-11-25 13:45:35 -0800

    styles

--- src/components/SettingsList/SettingsList.styles.tsx
+++ src/components/SettingsList/SettingsList.styles.tsx
@@ -10,7 +10,7 @@ export const ListItem = styled.li`
   padding: 1rem;
   margin: 0.2rem 0 1rem 0;
   border-radius: 8px;
-  border: solid 1px #ddd;
+  border: solid 1px #aaa;
 `;
 
 export const ListContainer = styled.div`
